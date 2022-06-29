38ada039c3c3c61df19e0a7f265818007cf310d4
Zoila <zoila.aliaga.87@gmail.com>
2022-06-29 09:41:09 -0500

    mostrando nombre de un objeto y probando consola

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-import { example } from './data.js';
+import { example, anotherExample } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+console.log(example());
+console.log(anotherExample());
+console.log(data.pokemon[1].img);
+console.log(data.pokemon[1].name);
+
+let root= document.querySelector('#root');
+let nombre= document.createElement("div");
+nombre.innerHTML=data.pokemon[1].name;
+root.appendChild(nombre);
