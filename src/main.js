334c260c2f0c95aca175214125197ae0cb638643
Zoila <zoila.aliaga.87@gmail.com>
2022-06-30 16:44:20 -0500

    Creando caja principal y agregandole estilo con css

--- src/main.js
+++ src/main.js
@@ -10,10 +10,19 @@ console.log(data.pokemon[1].name);
 
 let root= document.querySelector('#root');
 data.pokemon.forEach((pokemon)=>{
+    //creando caja
+    let cajaPrincipal=document.createElement("div")
+    cajaPrincipal.classList.add("cardPokemon");    
+    //crear imagen
     let imagen=document.createElement("img")
     imagen.src=pokemon.img;
+    //creando nombre
     let nombre= document.createElement("div");
-nombre.innerHTML=pokemon.name;
-root.appendChild(imagen);
-root.appendChild(nombre);
+    nombre.innerHTML=pokemon.name;
+    //contener los datos dentro de la caja
+    cajaPrincipal.appendChild(imagen)
+    cajaPrincipal.appendChild(nombre)
+    //agregarlo al root     
+    root.appendChild(cajaPrincipal);
+    
 })
