f48fdf59b28af7565c0e4f319c4298b18a1cf9b4
Herly <herlyyidam@gmail.com>
2022-07-12 12:42:33 -0500

    cambios antes del git restore

--- src/main.js
+++ src/main.js
@@ -5,33 +5,6 @@ import { filterData } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 let root= document.querySelector('#root');
-<<<<<<< HEAD
-data.pokemon.forEach((pokemon)=>{
-    //creando caja
-    let cajaPrincipal=document.createElement("div")
-    cajaPrincipal.classList.add("cardPokemon");
-    //creando imagen
-    let imagen=document.createElement("img")
-    imagen.src=pokemon.img;
-    //creando número
-    let numero= document.createElement("div");
-    numero.innerHTML= "#"+ pokemon.num;
-    //creando nombre
-    let nombre= document.createElement("div");
-    nombre.innerHTML= pokemon.name;
-    //creando tipo
-    let tipo= document.createElement("div");
-    tipo.innerHTML=pokemon.type;
-    //contener los datos dentro de la caja
-    cajaPrincipal.appendChild(imagen) 
-    cajaPrincipal.appendChild(numero)
-    cajaPrincipal.appendChild(nombre)
-    cajaPrincipal.appendChild(tipo)
-    //agregarlo al root
-    root.appendChild(cajaPrincipal);
-    
-})
-=======
 let showPokemon= ()=>{
     data.pokemon.forEach((pokemon)=>{
         //creando caja
@@ -69,4 +42,3 @@ let newArray=data.pokemon.filter((type)=>{
 
     
 console.log(filterData(8, 10))
->>>>>>> 1cf98ad29b859c2340fe2b8f365f8270daf8a46e
